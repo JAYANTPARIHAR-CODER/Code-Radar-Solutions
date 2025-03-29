1df6f3bc52b1e18cbf287f976e592114b1495b8d
JAYANT PARIHAR <JAYANTPARIHAR170@GMAIL.COM>
2025-03-29 22:50:18 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -2,6 +2,5 @@ int fibonacciSeries(int n){
     if(n==0) return 0;
     if(n==1) return 1;
     int Ansd= fibonacciSeries(n-1) + fibonacciSeries(n-2);
-    printf("%d", Ansd);
-    //return Ansd;
+    return Ansd;
 }
